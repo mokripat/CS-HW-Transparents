f9dba20bc4548fce97581d938b2baac2f262358d
Patrik Mokriš <patrik.mokris@etnetera.cz>
2025-08-01 10:19:52 +0200

    Add Colors, set Theme

--- app/src/main/java/cz/mokripat/transparents/ui/theme/Color.kt
+++ app/src/main/java/cz/mokripat/transparents/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package cz.mokripat.transparents.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue80 = Color(0xFFB5C4FA)
+val Blue40 = Color(0xFF2C51D4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueAccent80 = Color(0xFFCED9FB)
+val BlueAccent40 = Color(0xFF627BDB)
+
+val LightBackground80 = Color(0xFFF9FAFF)
+val LightSurface80 = Color(0xFFFFFFFF)
+val LightVariant80 = Color(0xFFE2E5F1)
+
+val OnPrimary80 = Color(0xFFFFFFFF)
+val OnBackground80 = Color(0xFF1B1C1F)
+val Outline80 = Color(0xFF9093A1)
+
+val Error80 = Color(0xFFF2B8B5)
+val Error40 = Color(0xFFB3261E)
