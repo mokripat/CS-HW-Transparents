db150dcb6218bb781e8501b21b96172df72ced40
Patrik Mokriš <patrik.mokris@etnetera.cz>
2025-08-01 11:13:55 +0200

    UI Dimensions object

---
+++ app/src/main/java/cz/mokripat/transparents/ui/theme/Dimensions.kt
@@ -0,0 +1,13 @@
+package cz.mokripat.transparents.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    val spacingXSmall = 4.dp
+    val spacingSmall = 8.dp
+    val spacingMedium = 16.dp
+
+    val buttonRadius = 12.dp
+}
+
+typealias Dim = Dimensions
